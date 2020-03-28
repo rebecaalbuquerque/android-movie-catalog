64482732f8d29eb6bbcd84ed5bb8f334ca7a27e0
rebecaalbuquerque <rebeca.albuquerque96@gmail.com>
2020-03-27 21:51:55 -0300

    Correções

--- app/src/main/java/com/albuquerque/moviecatalog/app/view/activity/MainActivity.kt
+++ app/src/main/java/com/albuquerque/moviecatalog/app/view/activity/MainActivity.kt
@@ -3,48 +3,19 @@ package com.albuquerque.moviecatalog.app.view.activity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.observe
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.albuquerque.moviecatalog.R
-import com.albuquerque.moviecatalog.app.viewmodel.MoviesViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MainActivity : AppCompatActivity() {
-
-    private val moviesViewModel: MoviesViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        subscribeUI()
         setupViews()
-
-    }
-
-    private fun subscribeUI() {
-
-        with(moviesViewModel) {
-            popular.observe(this@MainActivity) {
-                it
-            }
-
-            nowPlaying.observe(this@MainActivity) {
-                it
-            }
-
-            topRated.observe(this@MainActivity) {
-                it
-            }
-
-            latest.observe(this@MainActivity) {
-                it
-            }
-
-        }
 
     }
 
--- app/src/main/java/com/albuquerque/moviecatalog/app/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/albuquerque/moviecatalog/app/view/fragment/HomeFragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.albuquerque.moviecatalog.R
+import com.albuquerque.moviecatalog.app.adapter.MoviesAdapter
+import com.albuquerque.moviecatalog.app.viewmodel.MoviesViewModel
 import kotlinx.android.synthetic.main.fragment_home.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
+
+    private val moviesViewModel: MoviesViewModel by viewModel()
+    private val moviesAdapter: MoviesAdapter = MoviesAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
@@ -18,6 +25,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        subscribeUI()
         setupView()
     }
 
@@ -27,6 +35,31 @@ class HomeFragment : Fragment() {
         }
 
         activity?.let { slider.setup(arrayListOf("https://i.pinimg.com/736x/5e/62/b6/5e62b67f13ee0d57d7789945493de45b.jpg", "https://i.pinimg.com/736x/5e/62/b6/5e62b67f13ee0d57d7789945493de45b.jpg", "https://i.pinimg.com/736x/5e/62/b6/5e62b67f13ee0d57d7789945493de45b.jpg")) }
+
+        rvPopular.adapter = moviesAdapter
+
+    }
+
+    private fun subscribeUI() {
+
+        with(moviesViewModel) {
+            popular.observe(this@HomeFragment) { list ->
+                list?.let { moviesAdapter.refresh(it) }
+            }
+
+            nowPlaying.observe(this@HomeFragment) {
+                it
+            }
+
+            topRated.observe(this@HomeFragment) {
+                it
+            }
+
+            latest.observe(this@HomeFragment) {
+                it
+            }
+
+        }
 
     }
 
